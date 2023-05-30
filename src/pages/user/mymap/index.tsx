169fd64e0253df269dc4f4ee838680b0d0f40810
smp6340 <smp6340@naver.com>
2023-05-30 23:30:58 +0900

    UI :: 퍼블리싱

--- src/pages/user/mymap/index.tsx
+++ src/pages/user/mymap/index.tsx
@@ -1,40 +1,103 @@
-import react, { useState, useEffect } from "react";
-import { Map, MapMarker } from "react-kakao-maps-sdk";
+import React, { useState, useCallback, useEffect, useRef } from "react";
+import { Map, MapMarker, ZoomControl, MapTypeControl } from "react-kakao-maps-sdk";
 
 // Styles
 import * as S from "./style";
 
-function MyPage():React.ReactElement {
+// Images
+import Maker from "../../../assets/images/marker.png";
+import Menu from "../../../assets/images/menu.png";
+import Custom from "../../../assets/images/equalizer.png";
+import Comment from "../../../assets/images/comment.png";
+import { text } from "stream/consumers";
+
+function MyPage(): React.ReactElement {
     const [isOpen, setIsOpen] = useState(false);
     const [isClick, setIsClick] = useState(false);
     const [position, setPosition] = useState<any>();
+
     const toggleSide = () => {
         setIsOpen(true);
-    }
+    };
 
-    const newMapPin = () => {
-        setIsClick(true);
-    }
+    const toggleBtn = () => {
+        setIsClick(!isClick);
+    };
 
-    const handleMapPin = (_t: any, mouseEvent: { latLng: { getLat: () => any; getLng: () => any; }; }) => {
-        setPosition({
-            lat: mouseEvent.latLng.getLat(),
-            lng: mouseEvent.latLng.getLng(),
-        })
-    }
-    return(
-        <Map
-            center={{ lat: 36.39151269716873, lng: 127.36332665035195 }}
-            style={{ width: "100%", height: "100vh" }}
-            onClick={handleMapPin}
-            level={3}
-            maxLevel={10}
-            >
-            <S.MapPinBtn>
-                {position && <MapMarker position={position} />}
-            </S.MapPinBtn>
-        </Map>
-    )
-} 
+    const handleMapPin = (mouseEvent: any) => {
+        if (isClick && mouseEvent.latLng) {
+            const lat = mouseEvent.latLng.getLat();
+            const lng = mouseEvent.latLng.getLng();
+            setPosition({
+                lat: lat,
+                lng: lng,
+            });
+        }
+    };
+
+    // const handleSearch = useCallback(
+    //     (event: React.ChangeEvent<HTMLInputElement>) => {
+    //         const keyword = event.target.value;
+    //         const filteredList = list.filter((item) =>
+    //             item.title.toLowerCase().includes(keyword.toLowerCase())
+    //         );
+    //         setInterval(filteredList);
+    //     },
+    //     [list]
+    // );
+
+    return (
+        <>
+            <S.Page>
+                <S.MapPinBtn onClick={toggleBtn}>
+                    <S.DefaultMaker src={Maker} />
+                </S.MapPinBtn>
+                <S.CustomMapPinBtn onClick={toggleBtn}>
+                    <S.CustomMaker src={Custom} />
+                </S.CustomMapPinBtn>
+                <S.CommentBtn onClick={toggleBtn}>
+                    <S.CommentImg src={Comment} />
+                </S.CommentBtn>
+                <S.SideConents>
+                    <S.SearchContainer>
+                        <S.Search type={"text"} placeholder="카테고리를 검색하세요." />
+                        <S.ShareBtn>공유하기</S.ShareBtn>
+                    </S.SearchContainer>
+                    <S.CategoryContainer>
+                        <S.Category>
+                            <S.CategoryTitle>은행동 오락실</S.CategoryTitle>
+                            <S.CategoryUser>박상민</S.CategoryUser>
+                        </S.Category>
+                        <S.Category>
+                            <S.CategoryTitle>내 집 주변 카페</S.CategoryTitle>
+                            <S.CategoryUser>익명</S.CategoryUser>
+                        </S.Category>
+                        <S.Category>
+                            <S.CategoryTitle>서울 스타트업 목록</S.CategoryTitle>
+                            <S.CategoryUser>testuser01</S.CategoryUser>
+                        </S.Category>
+                        <S.Category>
+                            <S.CategoryTitle>전국 관광지 목록</S.CategoryTitle>
+                            <S.CategoryUser>testcase02</S.CategoryUser>
+                        </S.Category>
+                    </S.CategoryContainer>
+                </S.SideConents>
+                <Map
+                    center={{ lat: 36.39151269716873, lng: 127.36332665035195 }}
+                    style={{ width: "80vw", height: "100vh", margin: "0px" }}
+                    onClick={handleMapPin}
+                    level={3}
+                    maxLevel={10}
+                >
+                    {position && position.lat && position.lng && (
+                        <MapMarker position={position} />
+                    )}    
+                    <ZoomControl position={kakao.maps.ControlPosition.RIGHT} />
+                    <MapTypeControl position={kakao.maps.ControlPosition.TOPRIGHT} />
+                </Map>
+            </S.Page>
+        </>
+    );
+}
 
 export default MyPage;
--- src/pages/user/mymap/style.ts
+++ src/pages/user/mymap/style.ts
@@ -1,14 +1,196 @@
 import styled from "styled-components";
+import "../../../assets/fonts/font.css";
 
 export const Page = styled.div`
     width: 100%;
     height: 100vh;
 `;
 
+export const ShareBtn = styled.div`
+    position: absolute;
+    width: 40%;
+    height: 30px;
+    background-color: #8362C3;
+    border-radius: 7px;
+    box-shadow: 1px 1px 3px 1px #dadce0;
+    font-family: 'ONEPOP';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    :hover{
+        cursor: pointer;
+    }
+`;
+
+export const SideConents = styled.div`
+    width: 20vw;
+    height: 100%;
+    position: absolute;
+    background-color: #fff;
+    right: 0%;
+`;
+
+export const SearchContainer = styled.div`
+    width: 100%;
+    height: 15%;
+    background-color: #8362C3;
+    box-shadow: 1px 1px 3px 1px #dadce0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const CategoryContainer = styled.div`
+    width: 100%;
+    height: 85%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const Category = styled.div`
+    width: 90%;
+    height: 10%;
+    box-shadow: 1px 3px 3px 1px #dadce0;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-top: 20px;
+    border: 1px solid #ccc;
+`;
+
+export const CategoryTitle = styled.div`
+    font-family: 'ONEPOP';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    margin-left: 30px;
+`;
+
+export const CategoryUser = styled.div`
+    font-family: 'ONEPOP';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    margin-left: 35px;
+`;
+
+export const Search = styled.input`
+    width: 90%;
+    height: 30%;
+    background-color: #fff;
+    box-shadow: 3px 3px hsl(0deg 0% 0% / 0.38);
+    border: none;
+    border-radius: 7px;
+    text-align: center;
+    font-family: 'ONEPOP';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    outline: none;
+`;
+
 export const MapPinBtn = styled.div`
     position: absolute;
     width: 50px;
     height: 50px;
+    border: 1px solid #ccc;
+    border-radius: 10px;
     background-color: #fff;
+    box-shadow: 1px 1px 3px 1px #dadce0;
     z-index: 2;
+    top: 50%;
+    left: 7px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    :hover{
+        cursor: pointer;
+    }
+    :active{
+        box-shadow: 1px 1px 3px 1px #dadce0 inset;
+    }
+`;
+
+export const DefaultMaker = styled.img`
+    width: 30px;
+    height: 42px;
+    text-align: center;
+    position: absolute;
+`;
+
+export const CustomMapPinBtn = styled.div`
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    background-color: #fff;
+    box-shadow: 1px 1px 3px 1px #dadce0;
+    z-index: 2;
+    top: 44%;
+    left: 7px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    :hover{
+        cursor: pointer;
+    }
+    :active{
+        box-shadow: 1px 1px 3px 1px #dadce0 inset;
+    }
+`;
+
+export const CustomMaker = styled.img`
+    width: 30px;
+    height: 30px;
+    text-align: center;
+    position: absolute;
+`;
+
+export const CommentBtn = styled.div`
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    background-color: #fff;
+    box-shadow: 1px 1px 3px 1px #dadce0;
+    z-index: 2;
+    top: 38%;
+    left: 7px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    :hover{
+        cursor: pointer;
+    }
+    :active{
+        box-shadow: 1px 1px 3px 1px #dadce0 inset;
+    }
+`;
+
+export const CommentImg = styled.img`
+    width: 30px;
+    height: 30px;
+    text-align: center;
+    position: absolute;
+`;
+
+export const MenuBtn = styled.div`
+    width: 30px;
+    height: 30px;
+    z-index: 3;
+`;
+
+export const MenuImg = styled.img`
+    width: 30px;
+    height: 30px;
+    z-index: 3;
 `;
