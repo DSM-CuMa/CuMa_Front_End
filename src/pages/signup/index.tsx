5f107eba13e404b574a18f0adb32fd273f3831af
smp6340 <smp6340@naver.com>
2023-06-19 14:45:37 +0900

    ADD :: Sign Up Message

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -15,22 +15,70 @@ function SignUpPage():React.ReactElement {
     const [accountId, setAccountId] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [checkPassword, setCheckPassword] = useState('');
     const navigate = useNavigate();
 
+    const Toast = Swal.mixin({   
+        toast: true,   
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {     
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)  
+        }
+    })  
+
     async function SignUp() {
-        axios.post("http://localhost:3001/signup", {
-            accountId: accountId,
-            email: email,
-            password: password
-        })
-        .then((res) => {
-            console.log(res);
-            navigate("/login", {replace: true});
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+        if(accountId && password && email) {
+            if(password === checkPassword) {
+                if(accountId.length >= 6 && password.length >= 6) {
+                    var regId =  /^[a-zA-Z0-9]*$/;
+                    if(regId.test(accountId)) {
+                        var regEmail = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+$/;
+                        if(!regEmail.test(email)) {
+                            var regPassword = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/;
+                            if(!regPassword.test(password)) {
+                                axios.post("http://localhost:3001/signup", {
+                                    id: accountId,
+                                    email: email,
+                                    password: password
+                                })
+                                .then((res) => {
+                                    console.log(res);
+                                    navigate("/login", {replace: true});
+                                    Toast.fire({ icon: 'success', title: '회원가입에 성공했습니다' })
+                                })
+                                .catch((err) => {
+                                    Toast.fire({ icon: 'error', title: '회원가입에 실패했습니다' })
+                                })
+                            }   
+                            else {
+                                Toast.fire({ icon: 'error', title: '비밀번호가 형식에 맞지 않습니다.' })
+                            }
+                        }   
+                        else {
+                            Toast.fire({ icon: 'error', title: '이메일이 형식에 맞지 않습니다.' })
+                        }
+                    }
+                    else {
+                        Toast.fire({ icon: 'error', title: '아이디가 형식에 맞지 않습니다.' })
+                    }
+                }
+                else {
+                    Toast.fire({ icon: 'error', title: '아이디와 비밀번호는 모두 6자 이상이어야 합니다' })
+                }
+            }
+            else {
+                Toast.fire({ icon: 'error', title: '비밀번호가 일치하지 않습니다' })
+            }
+        }
+        else {
+            Toast.fire({ icon: 'error', title: '정보를 모두 입력해주세요' })
+        }
     }
+    
     return(
         <S.Page>
             <S.SignUpWrapper>
@@ -41,6 +89,8 @@ function SignUpPage():React.ReactElement {
                 <L.InputWrapper>
                     <L.InputName>아이디</L.InputName>
                     <L.Input
+                        value={accountId}
+                        onChange={(e)=>setAccountId(e.target.value)}
                         type={"text"}
                         placeholder="아이디를 입력해주세요."
                     />
@@ -48,6 +98,8 @@ function SignUpPage():React.ReactElement {
                 <L.InputWrapper>
                     <L.InputName>이메일</L.InputName>
                     <L.Input 
+                        value={email}
+                        onChange={(e)=>setEmail(e.target.value)}
                         type={"email"}
                         placeholder="이메일을 입력해주세요."
                     />
@@ -55,6 +107,8 @@ function SignUpPage():React.ReactElement {
                 <L.InputWrapper>
                     <L.InputName>비밀번호</L.InputName>
                     <L.Input 
+                        value={password}
+                        onChange={(e)=>setPassword(e.target.value)}
                         type={"password"}
                         placeholder="비밀번호를 입력해주세요."
                     />
@@ -62,12 +116,14 @@ function SignUpPage():React.ReactElement {
                 <L.InputWrapper>
                     <L.InputName>비밀번호 재입력</L.InputName>
                     <L.Input 
+                        value={checkPassword}
+                        onChange={(e)=>setCheckPassword(e.target.value)}
                         type={"password"}
                         placeholder="비밀번호를 다시 입력해주세요."
                     />
                 </L.InputWrapper>
                 <L.BtnWrapper>
-                    <L.SubmitBtn>확인</L.SubmitBtn>
+                    <L.SubmitBtn onClick={()=>SignUp()}>확인</L.SubmitBtn>
                 </L.BtnWrapper>
             </S.SignUpWrapper>
             <L.MapImage>
