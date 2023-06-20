b188ec2b8b0e8b4e6579606c0f5312d23ff09845
smp6340 <smp6340@naver.com>
2023-06-20 13:21:23 +0900

    ADD :: Cookie 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Route, Routes } from "react-router-dom";
+import axios from "axios";
+import {Cookies} from 'react-cookie'
 
 // Pages
 import MainPage from "./pages/main";
@@ -8,6 +10,30 @@ import SignUpPage from "./pages/signup";
 import MyPage from "./pages/user/mymap";
 
 function App() {
+
+  axios.defaults.baseURL = "http://localhost:3001";
+
+  const cookies = new Cookies();
+
+  const setCookie = (name: string, value: string, options?: any) => {
+    return cookies.set(name, value, {...options}); 
+  }
+
+  const getCookie = (name: string) => {
+    return cookies.get(name); 
+  }
+  
+  useEffect(()=>{
+    if(getCookie("atk")) 
+      axios.defaults.headers.common['Authorization'] = `Bearer ${getCookie("atk")}`
+    else if(getCookie("rtk"))
+      axios.post("/token/refresh", {refresh_token: getCookie("rtk")})
+        .then(res => {
+          setCookie("rtk", res.data.access_token);
+          axios.defaults.headers.common['Authorization'] = `Bearer ${res.data.access_token}`
+        })
+  },[])
+
   return (
     <>
       <Routes>
