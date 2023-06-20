71c954620d346625de12336369225462904b600d
smp6340 <smp6340@naver.com>
2023-06-20 13:22:48 +0900

    ADD :: 마커 추가, 수정, 삭제, 주소 검색 기능, 카테고리 불러오기 기능 추가

--- src/pages/user/mymap/index.tsx
+++ src/pages/user/mymap/index.tsx
@@ -1,105 +1,375 @@
-import React, { useState, useCallback, useEffect, useRef } from "react";
-import { Map, MapMarker, ZoomControl, MapTypeControl } from "react-kakao-maps-sdk";
+import React, { useState, useEffect, useRef } from "react";
+import { Map, MapMarker, ZoomControl, MapTypeControl, Roadview } from "react-kakao-maps-sdk";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 // Styles
 import * as S from "./style";
 
 // Images
-import Maker from "../../../assets/images/marker.png";
-import Menu from "../../../assets/images/menu.png";
-import Custom from "../../../assets/images/equalizer.png";
-import Comment from "../../../assets/images/comment.png";
-import { text } from "stream/consumers";
+import Category from "../../../assets/images/cateogory.svg";
+import Create from "../../../assets/images/create.svg";
+import Search from "../../../assets/images/search.svg";
+import MapPin from "../../../assets/images/mappin.png";
+import MapPin2  from "../../../assets/images/mappin2.png";
+import Add from "../../../assets/images/add.svg";
+import Del  from "../../../assets/images/del.svg";
+import Edit from "../../../assets/images/edit.svg";
+import Info from "../../../assets/images/info.svg";
+
+interface locationProps {
+    title: string,
+    info: string,
+    position: {
+        lat: number,
+        lng: number
+    }
+}
+
+interface categoryProps {
+    title: string,
+    user?: string,
+    locations: locationProps[]
+}
 
 function MyPage(): React.ReactElement {
-    const [isOpen, setIsOpen] = useState(false);
-    const [isClick, setIsClick] = useState(false);
-    const [position, setPosition] = useState<any>();
-    const [latitude, setLatitude] = useState<any>();
-    const [longitude, setLongitude] = useState<any>();
 
-    kakao.maps.event.addListener(map, 'click', function(mouseEvent: { latLng: any; }){
-        const latLng = mouseEvent.latLng;
-        setLatitude(latLng.getLat())
-        setLongitude(latLng.getLng())
+    const [menu, setMenu] = useState<string>("");
+    const [mode, setMode] = useState<string>("");
+    const [categories, setCategories] = useState<categoryProps[]>([]);
+    const [locations, setLocations] = useState<locationProps[]>([]);
+    const [selectedMaker, setSelectedMaker] = useState<any>(-1);
+
+    //현재위치 가져오기
+    const [location, setLoacation] = useState<any>({latitude: 37.5776087830657, longitude: 126.976896737645});
+
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition(getLocationSuccess);
+	}, [])
+
+	const getLocationSuccess = (response: any) => {
+		console.log(response);
+		const { latitude, longitude } = response.coords;
+		setLoacation({ latitude, longitude });
+	};
+    //
+
+    //맵 클릭 함수
+    const handleMap = (mouseEvent: any) => {
         
-    })
-    const toggleSide = () => {
-        setIsOpen(true);
-    };
+        const lat = mouseEvent.latLng.getLat();
+        const lng = mouseEvent.latLng.getLng();
 
-    const toggleBtn = () => {
-        setIsClick(!isClick);
-    };
-
-    const handleMapPin = (mouseEvent: any) => {
-        if (isClick && mouseEvent.latLng) {
-            const lat = mouseEvent.latLng.getLat();
-            const lng = mouseEvent.latLng.getLng();
-            setPosition({
-                lat: lat,
-                lng: lng,
-            });
+        if(mode === "add" && menu === "create") {
+            const checkSame = (e: any) => {
+                if(e.position.lat === lat && e.position.lng === lng)
+                    return true;
+            }
+            const already  = locations.some(checkSame)
+            if(!already)
+                setLocations([...locations, {title: "", info:"", position:{lat: lat, lng: lng}}])
+            setSelectedCategory(-1);
         }
     };
 
-    // const handleSearch = useCallback(
-    //     (event: React.ChangeEvent<HTMLInputElement>) => {
-    //         const keyword = event.target.value;
-    //         const filteredList = list.filter((item) =>
-    //             item.title.toLowerCase().includes(keyword.toLowerCase())
-    //         );
-    //         setInterval(filteredList);
-    //     },
-    //     [list]
-    // );
+    //마커 클릭 함수
+    const handleMapMaker = (idx: number) => {
+        console.log(idx);
+        if(mode === "del" && menu === "create") {
+            if(idx === selectedMaker)
+                setSelectedMaker(-1);
+            else if(selectedMaker > idx)
+                setSelectedMaker(selectedMaker-1);
+            setLocations(locations.filter((i, index) => index !== idx))
+            setSelectedCategory(-1);
+        }
+        else if(mode === "edit" || mode === "" || menu !== "create") {
+            setSelectedMaker(idx);
+            setMenu("info")
+        }
+    }
+
+    const changeMenu = (e: string) => {
+        if(menu === e)
+            setMenu("");
+        else
+            setMenu(e);
+    }
+
+    const changeMode = (e: string) => {
+        if(mode === e)
+            setMode("");
+        else
+            setMode(e);
+    }
+
+    //주소 검색
+    const [adressList, setAdressList] = useState<any[]>([]);
+
+    const kakaoToken = "7065b28ba1ac02a1ac133a6afe786462"
+
+    function getList(keyword: string) {
+        axios.get(`https://dapi.kakao.com/v2/local/search/keyword.json?query=${keyword}`, {headers: {Authorization: `KakaoAK ${kakaoToken}`}})
+        .then((res: any) => {
+            const list : any[] = [];
+            res.data.documents.map((i: any)=> {
+            list.push({name: i.place_name, adressName: i.address_name, x: i.x, y: i.y})
+            })
+            setAdressList(list);
+            console.log(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    } 
+
+    const searchHandleOnKeyPress = (e: any) => {
+        if (e.key === 'Enter') {
+            getList(e.target.value)
+        }
+    };
+
+    //토스트
+    const Toast = Swal.mixin({   
+        toast: true,   
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {     
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)  
+        }
+    })  
+
+    useEffect(()=>{
+        getCategoryList();
+    },[])
+
+    const getCategoryList = (keyword?: string) => {
+        axios.get(`/category${keyword ? `?q=${keyword}` : ""}`)
+            .then(res => {
+                console.log(res.data);
+                setCategories(res.data);
+            })
+            .catch(err => {
+                Toast.fire({ icon: 'error', title: '불러오는데 실패했습니다' })
+            })
+    }
+
+    const categoryHandleOnKeyPress = (e: any) => {
+        if (e.key === 'Enter') {
+            getCategoryList(e.target.value)
+        }
+    }
+
+    const moveToAdress = (i: any) => {
+        setLoacation({latitude: Number(i.y), longitude: Number(i.x)})
+        const checkSame = (e: any) => {
+            if(e.position.lat === Number(i.y) && e.position.lng === Number(i.x))
+                return true;
+        }
+        const already  = locations.some(checkSame)
+        if(!already)
+            setLocations([...locations, {title: i.name, info: "", position: {lat: Number(i.y), lng: Number(i.x)}}])
+    }
+
+    const onChangeInfo = (e: any) => {
+        const { name, value } = e.target;
+        let loc = locations[selectedMaker];
+        loc = {...loc, [name]: value};
+        console.log(loc);
+        let list = locations;
+        list[selectedMaker] = loc;
+        setLocations(list);
+        setSelectedCategory(-1);
+    }
+
+    //카테고리 등록
+    const postCategory = () => {
+        Swal.fire({
+            title: '카테고리 등록',
+            text: '카테고리를 등록 하시겠습니까?',
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: '확인',
+            cancelButtonText: '취소',
+            reverseButtons: true,
+        })
+        .then(result => {
+            if(locations.length >= 3) {
+                if(titleRef.current?.value) {
+                    axios.post("/category", {title: titleRef.current?.value, locations: locations})
+                        .then(res => {
+                            Toast.fire({ icon: 'success', title: '성공적으로 등록했습니다' })
+                            getCategoryList();
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            if(err.response.status === 409)
+                                Toast.fire({ icon: 'error', title: '중복되는 카테고리명 입니다' })
+                            else
+                                Toast.fire({ icon: 'error', title: '등록에 실패했습니다' })
+                        })
+                }
+                else
+                    Toast.fire({ icon: 'error', title: '제목을 입력해주세요' })
+            }
+            else
+                Toast.fire({ icon: 'error', title: '최소 3개 이상의 마커를 등록해주세요' })
+        });
+    }
+
+    const [selectedCategory, setSelectedCategory] = useState<number>(-1);
+
+    const selectCategory = (clocations: locationProps[], idx: number) => {
+        setSelectedCategory(idx);
+        setSelectedMaker(-1);
+        setLocations([...clocations]);
+    }
+
+    const titleRef = useRef<HTMLInputElement | null>(null);
+
+    const SideBarContent = () => {
+        switch(menu) {
+            case "create":
+                return (
+                    <S.CreateDiv>
+                        <S.ToolList>
+                        <S.SearchInputDiv>
+                            <input placeholder="카테고리 이름을 입력하세요." ref={titleRef}/>
+                        </S.SearchInputDiv>
+                            <S.Tool className="f" selected={mode==="add"} onClick={()=>changeMode("add")}>
+                                <div>
+                                    <img src={Add}/>
+                                </div>
+                                <span>마커 추가</span>
+                            </S.Tool>
+                            <S.Tool selected={mode==="edit"} onClick={()=>changeMode("edit")}>
+                                <div>
+                                    <img src={Edit}/>
+                                </div>
+                                <span>마커 수정</span>
+                            </S.Tool>
+                            <S.Tool selected={mode==="del"} onClick={()=>changeMode("del")}>
+                                <div>
+                                    <img src={Del}/>
+                                </div>
+                                <span>마커 삭제</span>
+                            </S.Tool>
+                        </S.ToolList>
+                        <S.PostBtn onClick={()=>postCategory()}>등록</S.PostBtn>
+                    </S.CreateDiv>
+                )
+            case "search":
+                return (
+                    <>
+                        <S.SearchInputDiv>
+                            <input placeholder="장소, 도로를 검색하세요." onKeyPress={searchHandleOnKeyPress}/>
+                        </S.SearchInputDiv>
+                        <S.AdressList>
+                            {adressList.map((i: any, idx: number) => (
+                                <li key={idx} onClick={()=>moveToAdress(i)}>
+                                    <span>{i.name}</span>
+                                    <span className="adress">{i.adressName}</span>
+                                </li>
+                            ))}
+                        </S.AdressList>
+                    </>
+                )
+            case "category":
+                return (
+                    <>
+                    <S.SearchContainer>
+                        <S.Search placeholder="카테고리를 검색하세요." onKeyPress={categoryHandleOnKeyPress}/>
+                    </S.SearchContainer>
+                    <S.CategoryContainer>
+                        {categories.map((category: categoryProps, idx: number) => (
+                            <S.Category selected={idx===selectedCategory} key={idx} onClick={()=>selectCategory(category.locations, idx)}>
+                                <S.CategoryTitle>{category.title}</S.CategoryTitle>
+                                <S.CategoryUser>{category.user}</S.CategoryUser>
+                            </S.Category>
+                        ))}
+                    </S.CategoryContainer>
+                    </>
+                )
+            case "info":
+                return (
+                    <S.InfoDiv>
+                        <Roadview 
+                            style={{ width: "100%", height: "255px", margin: "0px" }}
+                            position={{ lat: locations[selectedMaker].position.lat, lng: locations[selectedMaker].position.lng, radius: 30}}
+                        />
+                        <S.MarkerName placeholder="이름을 입력해주세요." onChange={(e)=>onChangeInfo(e)} name="title" defaultValue={locations[selectedMaker].title}/>
+                        <S.MarkerInfo placeholder="설명을 입력해주세요." onChange={(e)=>onChangeInfo(e)} name="info" defaultValue={locations[selectedMaker].info}/>
+                    </S.InfoDiv>
+                )
+            default:
+                return <></>
+        }
+    }
 
     return (
         <>
             <S.Page>
-                <S.MapPinBtn onClick={toggleBtn}>
-                    <S.DefaultMaker src={Maker} />
-                </S.MapPinBtn>
-                <S.CustomMapPinBtn onClick={toggleBtn}>
-                    <S.CustomMaker src={Custom} />
-                </S.CustomMapPinBtn>
-                <S.CommentBtn onClick={toggleBtn}>
-                    <S.CommentImg src={Comment} />
-                </S.CommentBtn>
-                <S.SideConents>
-                    <S.SearchContainer>
-                        <S.Search type={"text"} placeholder="카테고리를 검색하세요." />
-                        <S.ShareBtn>공유하기</S.ShareBtn>
-                    </S.SearchContainer>
-                    <S.CategoryContainer>
-                        <S.Category>
-                            <S.CategoryTitle>은행동 오락실</S.CategoryTitle>
-                            <S.CategoryUser>박상민</S.CategoryUser>
-                        </S.Category>
-                        <S.Category>
-                            <S.CategoryTitle>내 집 주변 카페</S.CategoryTitle>
-                            <S.CategoryUser>익명</S.CategoryUser>
-                        </S.Category>
-                        <S.Category>
-                            <S.CategoryTitle>서울 스타트업 목록</S.CategoryTitle>
-                            <S.CategoryUser>testuser01</S.CategoryUser>
-                        </S.Category>
-                        <S.Category>
-                            <S.CategoryTitle>전국 관광지 목록</S.CategoryTitle>
-                            <S.CategoryUser>testcase02</S.CategoryUser>
-                        </S.Category>
-                    </S.CategoryContainer>
-                </S.SideConents>
+                <S.MenuDiv>
+                    <div className="logo">
+                        <img src={MapPin}/>
+                    </div>
+                    <S.Menu selected={menu==="category"} onClick={()=>changeMenu("category")}>
+                        <div>
+                            <img src={Category}/>
+                        </div>
+                        <span>카테고리</span>
+                    </S.Menu>
+                    <S.Menu selected={menu==="search"} onClick={()=>changeMenu("search")}>
+                        <div>
+                            <img src={Search}/>
+                        </div>
+                        <span>지도 검색</span>
+                    </S.Menu>
+                    <S.Menu selected={menu==="create"} onClick={()=>changeMenu("create")}>
+                        <div>
+                            <img src={Create}/>
+                        </div>
+                        <span>만들기</span>
+                    </S.Menu>
+                    {selectedMaker !== -1 ?
+                        <S.Menu selected={menu==="info"} onClick={()=>changeMenu("info")}>
+                            <div>
+                                <img src={Info}/>
+                            </div>
+                            <span>정보</span>
+                        </S.Menu>
+                        :
+                        <></>
+                    }
+                </S.MenuDiv>
+                {menu !== "" ?
+                    <S.SideConents>
+                        <SideBarContent/>
+                    </S.SideConents>
+                    :
+                    <></>
+                }
                 <Map
-                    center={{ lat: 36.39151269716873, lng: 127.36332665035195 }}
-                    style={{ width: "80vw", height: "100vh", margin: "0px" }}
-                    onClick={handleMapPin}
+                    center={{ lat: location.latitude, lng: location.longitude }}
+                    style={{ width: "calc(100vw-63px)", height: "100vh", margin: "0px", position: "relative" }}
+                    onClick={(_t, mouseEvent) => handleMap(mouseEvent)}
                     level={3}
                     maxLevel={10}
                 >
-                    {position && position.lat && position.lng && (
-                        <MapMarker position={position} />
-                    )}    
+                    {locations.map((loc: locationProps, idx) => (
+                        <MapMarker 
+                            key={`${loc.title}-${loc.position}`}
+                            position={loc.position} 
+                            title={loc.title}
+                            image={{src: `${idx === selectedMaker ? MapPin2 : MapPin}`, size: {width: 25, height: 33}}}
+                            onClick={()=>handleMapMaker(idx)}
+                        />
+                    ))}
                     <ZoomControl position={kakao.maps.ControlPosition.RIGHT} />
                     <MapTypeControl position={kakao.maps.ControlPosition.TOPRIGHT} />
                 </Map>
@@ -108,4 +378,4 @@ function MyPage(): React.ReactElement {
     );
 }
 
-export default MyPage;
+export default React.memo(MyPage);
--- src/pages/user/mymap/style.ts
+++ src/pages/user/mymap/style.ts
@@ -11,33 +11,200 @@ export const ShareBtn = styled.div`
     width: 40%;
     height: 30px;
     background-color: #8362C3;
-    border-radius: 7px;
-    box-shadow: 0 2px 2px gray inset;
+    border-bottom-left-radius: 7px;
+    border-bottom-right-radius: 7px;
     font-family: 'ONEPOP';
     font-style: normal;
     font-weight: 400;
-    font-size: 16px;
+    font-size: 14px;
     color: #fff;
     display: flex;
     justify-content: center;
     align-items: center;
-    margin-top: 90px;
+    margin-top: 105px;
     :hover{
         cursor: pointer;
     }
 `;
 
-export const SideConents = styled.div`
-    width: 20vw;
+export const MenuDiv = styled.div`
+    width: 63px;
     height: 100%;
-    position: absolute;
+    display: flex;
+    flex-direction: column;
+    z-index: 1000;
+    position: fixed;
+    left: 0%;
+    background-color: white;
+    border-right: 1px solid rgba(0,0,0,.15);
+    .logo {
+        display: flex;
+        flex: 0 0 auto;
+        justify-content: center;
+        align-items: center;
+        height: 76px;
+        border-bottom: 1px solid rgba(0,0,0,.15);
+        box-sizing: border-box;
+        img{
+            width:  26px;
+            height: 33px;
+        }
+    }
+`
+
+interface MenuProps {
+    selected: boolean
+}
+
+export const Menu = styled.div<MenuProps>`
+    font-family: 'ONEPOP';
+    background-color: ${props => props.selected ? "#8362c3" : "white"};
+    color: ${props => props.selected ? "white" : "#333"};
+    fill: ${props => props.selected ? "white" : "#333"};
+    height: 60px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    img {
+        filter: ${props => props.selected ? "" : "invert(0%) sepia(19%) saturate(0%) hue-rotate(103deg) brightness(30%) contrast(103%);"};
+    }
+    div{
+        display: flex;
+        justify-content: center;
+    }
+    span {
+        display: block;
+        min-height: 13px;
+        margin-top: 3px;
+        font-size: 12px;
+        line-height: 15px;
+        letter-spacing: -.44px;
+        text-align: center;
+    }
+`
+
+export const SideConents = styled.div`
+    width: 25vw;
+    height: 100%;
+    position: fixed;
     background-color: #fff;
-    right: 0%;
+    left: 63px;
+    z-index: 999;
+    box-shadow: 10px 0px 10px -2px rgba(0, 0, 0, 0.2);
 `;
+
+export const CreateDiv = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+
+export const ToolList = styled.ul`
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    .f{
+        border-top: 1px solid rgba(0,0,0,.15);
+    }
+    /*
+    input{
+        font-family: 'ONEPOP';
+        font-size: 20px;
+        letter-spacing: 2px;
+        border: 0;
+        padding: 0px 15px;
+        width: 100%;
+        height: 75px;
+    }*/
+`
+
+interface ToolProps {
+    selected: boolean
+}
+
+export const Tool = styled.li<ToolProps>`
+    font-family: 'ONEPOP';
+    padding: 0px 15px;
+    height: 60.3px;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+    cursor: pointer;
+    background-color: ${props => props.selected ? "#8362c3" : "white"};
+    color: ${props => props.selected ? "white" : "#333"};
+    display: flex;
+    align-items: center;
+    div{
+        display: flex;
+        align-items: center;
+        height: 20px;
+        width: 20px;
+        margin-right: 7px;
+    }
+    img{
+        filter: ${props => props.selected ? "" : "invert(0%) sepia(19%) saturate(0%) hue-rotate(103deg) brightness(30%) contrast(103%);"};
+    }
+    :nth-child(1) {
+        border-top: 1px solid rgba(0, 0, 0, 0.2);
+    }
+`
+
+export const PostBtn = styled.button`
+    background-color: #8362c3;
+    color: white;
+    height: 44px;
+    border-radius: 8px;
+    margin: 15px;
+    font-size: 17px;
+    font-family: 'ONEPOP';
+`
+
+export const SearchInputDiv = styled.div`
+    margin: 16px;
+    border: 2px solid #8362C3;
+    height: 43px;
+    border-radius: 6px;
+    input {
+        font-family: 'ONEPOP';
+        font-size: 16px;
+        letter-spacing: 2px;
+        border: 0;
+        width: 100%;
+        border-radius: 6px;
+        height: 100%;
+        padding: 0 15px;
+    }
+`
+
+export const AdressList = styled.ul`
+    padding: 0;
+    list-style: none;
+    margin-top: 30px;
+    height: calc(100% - 90px);
+    overflow-y: auto;
+    font-family: 'ONEPOP';
+    li {
+        cursor: pointer;
+        padding: 15px 15px;
+        border-bottom: 1px solid #F4F4F4;
+        display: flex;
+        flex-direction: column;
+        div{
+            margin-top: 10px;
+            display: flex;
+        }
+        .adress {
+            margin-top: 5px;
+            color: #666;
+            font-size: 13px;
+        }
+    }
+`
 
 export const SearchContainer = styled.div`
     width: 100%;
-    height: 15%;
+    height: 76px;
     background-color: #8362C3;
     box-shadow: 1px 1px 3px 1px #dadce0;
     display: flex;
@@ -47,23 +214,36 @@ export const SearchContainer = styled.div`
 `;
 
 export const CategoryContainer = styled.div`
+    margin-top: 20px;
     width: 100%;
-    height: 85%;
     display: flex;
     flex-direction: column;
     align-items: center;
+    margin-top: 20px;
+    height: calc(100% - 95px);
+    overflow-y: auto;
 `;
 
-export const Category = styled.div`
+interface CategoryProps {
+    selected: boolean
+}
+
+export const Category = styled.div<CategoryProps>`
+    cursor: pointer;
     width: 90%;
-    height: 10%;
+    height: 80px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
     box-shadow: 1px 3px 3px 1px #dadce0;
-    border-radius: 10px;
+    border-radius: 7px;
     display: flex;
     flex-direction: column;
     justify-content: center;
     margin-top: 20px;
     border: 1px solid #ccc;
+    padding: 0px 30px;
+    background-color: ${props => props.selected ? "#e1d2ff" : "white"};
 `;
 
 export const CategoryTitle = styled.div`
@@ -71,20 +251,22 @@ export const CategoryTitle = styled.div`
     font-style: normal;
     font-weight: 400;
     font-size: 24px;
-    margin-left: 30px;
 `;
 
 export const CategoryUser = styled.div`
+    margin-top: 3px;
     font-family: 'ONEPOP';
     font-style: normal;
     font-weight: 400;
-    font-size: 14px;
-    margin-left: 35px;
+    font-size: 17px;
+    margin-left: 1px;
+    color: #999;
 `;
 
 export const Search = styled.input`
     width: 90%;
-    height: 30%;
+    height: 46%;
+    letter-spacing: 2px;
     background-color: #fff;
     box-shadow: 3px 3px hsl(0deg 0% 0% / 0.38);
     border: none;
@@ -93,7 +275,7 @@ export const Search = styled.input`
     font-family: 'ONEPOP';
     font-style: normal;
     font-weight: 400;
-    font-size: 24px;
+    font-size: 17px;
     outline: none;
 `;
 
@@ -195,3 +377,71 @@ export const MenuImg = styled.img`
     height: 30px;
     z-index: 3;
 `;
+
+export const Pallete = styled.div`
+    position: fixed;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 8px;
+    display: flex;
+    flex-direction: column;
+    z-index: 999;
+    border: 1px solid rgb(226, 226, 226);
+    border-top: 0;
+    border-radius: 2px;
+`
+
+interface PalleteButtonProps {
+    selected: boolean;
+}
+
+export const PalleteButton = styled.button<PalleteButtonProps>`
+    background-color: ${props => props.selected ? "#8362c3" : "white"};
+    border: 0;
+    border-radius: 0;
+    border-top: 1px solid rgb(226, 226, 226);
+    outline: 0;
+    width: 32px;
+    height: 32px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const InfoDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const MarkerName = styled.input`
+    margin: 0px 15px;
+    margin-top: 20px;
+    font-size: 15px;
+    height: 43px;
+    border: 2px solid #aaa;
+    font-family: "ONEPOP";
+    padding: 10px;
+    outline: 0;
+    border-radius: 5px;
+    letter-spacing: 1.5px;
+    :focus {
+        border: 2px solid #8362c3;
+    }
+`
+
+export const MarkerInfo = styled.textarea`
+    letter-spacing: 1.5px;
+    font-size: 15px;
+    border-radius: 5px;
+    height: 230px;
+    margin: 0px 15px;
+    margin-top: 15px;
+    resize: none;
+    border: 2px solid #aaa;
+    padding: 10px;
+    font-family: "ONEPOP";
+    outline: 0;
+    :focus {
+        border: 2px solid #8362c3;
+    }
+`
